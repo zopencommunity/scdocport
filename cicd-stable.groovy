a418d4fb579830aaeff252ad28bc29fe10bb5d80
sujata <sujatakdevi12@gmail.com>
2025-05-30 03:51:12 -0400

    zopen scdoc

---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/scdocport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/scdocport.git'), string(name: 'PORT_DESCRIPTION', value: 'scdoc is a simple man page generator for POSIX systems written in C99.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
